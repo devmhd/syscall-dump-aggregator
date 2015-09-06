30f7803007c39c3c90325ffdeb02874a180dcf8f
Mehedee Zaman <devmhd@gmail.com>
2015-09-06 09:20:30 +0600

    Add submitted dataset and run

--- src/com/devmhd/Main.java
+++ src/com/devmhd/Main.java
@@ -13,128 +13,117 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-	
-	
-	private HashMap<String,Integer> syscallFrequency = new HashMap<String,Integer>();
 
-	
+
 	public static ArrayList<String> removeEmptyStrings(String[] array){
 		ArrayList<String> list = new ArrayList<String>();
 		for(String str : array)
 			if(!str.isEmpty())
 				list.add(str);
-		
+
 		return list;
 	}
-	
-	public static void main(String[] args) {
-		
-		
-		HashMap<String,Integer> syscallFrequency = new HashMap<String,Integer>();
-		
-		
-		FileInputStream fStream;
-		BufferedReader reader;
+
+	public static HashMap<String,Integer> getFrequenciesFromFile(File file) throws NumberFormatException, IOException{
+		HashMap<String,Integer> map = new HashMap<String,Integer>();
+
+		FileInputStream fStream = new FileInputStream(file);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(fStream));
+
+		String line;
+		boolean readingNow = false;
+
+		int count = 0;
+
 		ArrayList<String> record;
 		String syscallname;
 		Integer n_calls;
+
+
+		while ((line = reader.readLine()) != null)   {
+
+			if(line.isEmpty()) continue;
+
+			if(line.startsWith("-")){
+				readingNow = ! readingNow;
+				continue;
+			}
+
+			if(readingNow){
+
+				record = removeEmptyStrings(line.split(" "));
+
+				syscallname = record.get(record.size()-1);
+				n_calls = Integer.parseInt(record.get(3));
+
+				map.put(syscallname, n_calls);
+
+				count++;	
+
+			}
+
+		}
 		
-		File trainingFolder = new File("training-data");
+		reader.close();
+
+		if(count==0) System.out.println("No syscall: " + file.getName());
+
+		return map;
+
+	}
+	
+	public static void aggregateFolder(File folder, String outputFileName){
 		
-		for(File dumpFile : trainingFolder.listFiles()){
-			
+		HashMap<String,Integer> syscallFrequency = new HashMap<String,Integer>(), singleAppFreqs;
+
+		int n_apps = 0;
+
+		for(File dumpFile : folder.listFiles()){
+
+			n_apps++;
+
 			try {
-				
-				fStream = new FileInputStream(dumpFile);
-				reader = new BufferedReader(new InputStreamReader(fStream));
-				
-				String line;
-				boolean readingNow = false;
-				
-				int count = 0;
 
-				while ((line = reader.readLine()) != null)   {
+				singleAppFreqs = getFrequenciesFromFile(dumpFile);
+				
+				for(Map.Entry<String, Integer> entry : singleAppFreqs.entrySet())
+				{
+					if(syscallFrequency.containsKey(entry.getKey())){
+			//			syscallFrequency.put(entry.getKey(), syscallFrequency.get(entry.getKey())+ entry.getValue() );
+						syscallFrequency.put(entry.getKey(), syscallFrequency.get(entry.getKey())+ 1 );
+					} else {
 
-					if(line.isEmpty()) continue;
-					
-					if(line.startsWith("-")){
-						readingNow = ! readingNow;
-						continue;
+			//			syscallFrequency.put(entry.getKey(), new Integer(entry.getValue()));
+						syscallFrequency.put(entry.getKey(), new Integer(1));
+
 					}
-					
-					if(readingNow){
-						
-		//				System.out.println(line);
-						record = removeEmptyStrings(line.split(" "));
-						
-						syscallname = record.get(record.size()-1);
-						n_calls = Integer.parseInt(record.get(3));
-						
-						
-						if(syscallFrequency.containsKey(syscallname)){
-							syscallFrequency.put(syscallname, syscallFrequency.get(syscallname)+ n_calls );
-						} else {
-							
-							syscallFrequency.put(syscallname, new Integer(n_calls));
-													
-						}
-						
-						
-					count++;	
-						
-					}
-					
-					
-
-
 				}
-				
-				System.out.println("" + count + " " + dumpFile.getName());
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-//				
+
+				//				
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}
-//			
-//			
-//			
-//			
-//			
+			}		
 		}
-		
-		
+
+
 		PrintWriter writer;
 		try {
-			writer = new PrintWriter("output.csv", "UTF-8");
-			
-			
-			
-			
-			
-			
+			writer = new PrintWriter(outputFileName, "UTF-8");
+
+
+			writer.println("" + n_apps);
+
 			for(Map.Entry<String, Integer> entry : syscallFrequency.entrySet())
 			{
 				String word = entry.getKey() + "," + entry.getValue();
-				
+
 				writer.println(word);
 			}
-			
+
 			writer.close();
-			
-			
+
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -142,9 +131,14 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
+	}
+
+	public static void main(String[] args) {
+
+		aggregateFolder(new File("training-data/good"), "output-good.csv");
+		aggregateFolder(new File("training-data/malware"), "output-malware.csv");
+
+
 	}
 
 }
